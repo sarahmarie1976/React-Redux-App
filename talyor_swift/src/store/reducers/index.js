95122199e01fe93f202e33a71a4a84858e8cf6df
Sarahmarie D Hollenstein <sholle7@gmail.com>
2020-08-13 23:18:55 -0500

    Revert "Sarahmarie hollenstein"

--- talyor_swift/src/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'; // this will combine all reducers if there is more then one
-import { quotesReducer as quotes } from './quotesReducer';
-
-
-export default combineReducers({
-	quotes,
-});
